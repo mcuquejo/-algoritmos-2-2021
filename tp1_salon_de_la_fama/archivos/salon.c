44d8855826dc77ca7a3a39f113299b7175cff057
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2021-05-03 22:39:35 -0300

    Ajustes tp ya no hay errores de memoria

--- tp1_salon_de_la_fama/archivos/salon.c
+++ tp1_salon_de_la_fama/archivos/salon.c
@@ -14,57 +14,46 @@ salon_t* crear_salon_vacio(){
     if(!salon)
         return NULL;
 
-    // salon->entrenadores = NULL;
-
     return salon;
 }
 
-void liberar_equipo(entrenador_t* entrenador) {
-    if (!entrenador->equipo)
-        return;
 
-    //entiendo que no deberia fallar, si el vtrlen es igual a 0
-    for(size_t i  = 0; i < vtrlen(entrenador->equipo); i++) {
-        //libero el nombre del pokemon
-        //free(entrenador->equipo[i]->nombre);
-        //libero al pokemon
-        free(entrenador->equipo[i]);
+void liberar_equipo(entrenador_t* entrenador) {
+    if (entrenador->equipo){
+        size_t tam = vtrlen(entrenador->equipo);
+        for(size_t i  = 0; i < tam; i++) {
+            free(entrenador->equipo[i]);
+        }
     }
-    //libero el array que contenia a los pokemones
     free(entrenador->equipo);
 }
 
+
 void liberar_entrenadores(salon_t* salon) {
-    if (!salon->entrenadores)
-        return;
-    //entiendo que no deberia fallar, si el vtrlen es igual a 0
-    for(size_t i = 0; i < vtrlen(salon->entrenadores); i++) {
-        //libero los pokemones del entrenador
-        liberar_equipo(salon->entrenadores[i]);
-        //libero el nombre del entrenador
-        //free(salon->entrenadores[i]->nombre);
+    if (salon->entrenadores){
+        size_t tam = vtrlen(salon->entrenadores);
+        for(size_t i = 0; i < tam; i++) {
+            liberar_equipo(salon->entrenadores[i]);
+            free(salon->entrenadores[i]);
+        }
     }
-    //libero el array que contenia a los entrenadores
     free(salon->entrenadores);
 }
 
+
 void liberar_salon(salon_t* salon) {
-    //libero entrenadores, junto con sus equipos.
     liberar_entrenadores(salon);
-    //libero el salon
     free(salon);
 }
+
 
 salon_t* cargar_entrenador(salon_t* salon, entrenador_t* entrenador) {
     void* entrenador_aux = vtradd(salon->entrenadores, entrenador);
     if (!entrenador_aux) {
-        //si falla la carga de un entrenador, libero el salon junto con los entrenadores previamente cargados y sus pokemones.
-        liberar_salon(salon);
         return NULL;
     }
 
-    //si no falló, apunto al nuevo vector dinamico de entrenadores
-    salon->entrenadores = entrenador_aux;
+    salon->entrenadores = (entrenador_t**)entrenador_aux;
     return salon;
 }
 
@@ -77,14 +66,13 @@ entrenador_t* crear_entrenador_vacio(char* nombre, int victorias) {
 
     entrenador->victorias = victorias;
 
-    //entrenador->equipo = NULL;
-
     return entrenador;
 }
 
 salon_t* cargar_pokemon(salon_t* salon, entrenador_t* entrenador, pokemon_t* pokemon) {
     void* pokemon_aux = vtradd(entrenador->equipo, pokemon);
     if(!pokemon_aux) {
+        free(pokemon);
         liberar_salon(salon);
         return NULL;
     }
@@ -113,91 +101,74 @@ pokemon_t* crear_pokemon(char* nombre,int nivel, int fuerza, int inteligencia, i
 
 
 salon_t* salon_leer_archivo(const char* nombre_archivo){
-
     FILE* archivo = fopen(nombre_archivo, "r");
-
     if(!archivo)
         return NULL;
 
-    //si pude leer el archivo, ya puedo crear un salon:
     salon_t* salon = crear_salon_vacio();
-    //leo una linea del archivo
-    char* linea = fgets_alloc(archivo);
-    if(!linea) {
-            fclosen(archivo);
-            return salon;
-        }
+    if(!salon) {
+        fclosen(archivo);
+        return NULL;
+    }
 
-    //mientras el archivo no haya llegado al final
+    char* linea = fgets_alloc(archivo);
+
     while(linea) {
-        //si retorna null, es porque esta leyendo una linea vacia, asi que cierro el archivo y retorno el salon creado.
         if(!linea) {
             fclosen(archivo);
             return salon;
         }
 
-        //si lei algo, tengo que splitearlo, para convertirlo en un array dinamico.
-        char** auxiliar = split(linea, ';');
-        //si no pudo generar el vector dinamico, cierro el archivo, libero la linea y retorno NULL.
-        //tener en cuenta que si leo más de una linea, acá seguramente tenga que liberar algo.
-        if(!auxiliar) {
+        char** vtr_linea_aux = split(linea, ';');
+        if(!vtr_linea_aux) {
+            liberar_salon(salon);
             free(linea);
             fclosen(archivo);
             return NULL;
         }
 
-        //ya tengo mi array. Segun la cantidad de elementos, se si es un entrenador o un pokemon.
-        //si tiene menos de dos elementos o mas de dos y menos de seis, o mas de seis, el formato del texto es incorrecto y corto la ejecucion
-        if((vtrlen(auxiliar) < 2) || (vtrlen(auxiliar) > 2 && vtrlen(auxiliar) < 6) || vtrlen(auxiliar) > 6) {
+        if((vtrlen(vtr_linea_aux) < 2) || (vtrlen(vtr_linea_aux) > 2 && vtrlen(vtr_linea_aux) < 6) || vtrlen(vtr_linea_aux) > 6) {
             free(linea);
-            vtrfree(auxiliar);
+            vtrfree(vtr_linea_aux);
             fclosen(archivo);
             return salon;
         }
 
-        //el primer elemento siempre deberia ser un entrenador. mientras haya entrenadores, recorro el texto
-        if (vtrlen(auxiliar) == 2) {
-            //si es un entrenador, lo creo, sin pokemones
-            char* nombre = auxiliar[0];
-            int victorias = atoi(auxiliar[1]);
+        if (vtrlen(vtr_linea_aux) == 2) {
+            char* nombre = vtr_linea_aux[0];
+            int victorias = atoi(vtr_linea_aux[1]);
+
             entrenador_t* entrenador = crear_entrenador_vacio(nombre, victorias);
-
             if (!entrenador) {
-                vtrfree(auxiliar);
+                vtrfree(vtr_linea_aux);
                 free(linea);
                 liberar_salon(salon);
                 fclosen(archivo);
                 return NULL;
             }
 
-            //ahora lo agrego al salon. Desde ahora voy a poder referenciar al entrenador desde el mismo salon
             salon_t* aux_salon = cargar_entrenador(salon, entrenador);
-
-            //si falla la carga del entrenador, libero todo y retorno NULL
             if(!aux_salon){
-                vtrfree(auxiliar);
+                liberar_equipo(entrenador);
+                free(entrenador);
+                vtrfree(vtr_linea_aux);
                 free(linea);
                 liberar_salon(salon);
                 fclosen(archivo);
                 return NULL;
             }
-            //sino, actualizo la referencia. No creo que esta linea sea necesaria.
-            salon = aux_salon;
+        } else if(vtrlen(vtr_linea_aux) == 6) {
+            char* nombre = vtr_linea_aux[0];
+            int nvl = atoi(vtr_linea_aux[1]);
+            int def = atoi(vtr_linea_aux[2]);
+            int fuer = atoi(vtr_linea_aux[3]);
+            int intl = atoi(vtr_linea_aux[4]);
+            int vel = atoi(vtr_linea_aux[5]);
 
-        }
-        else if(vtrlen(auxiliar) == 6) {
-            char* nombre = auxiliar[0];
-            int nvl = atoi(auxiliar[1]);
-            int def = atoi(auxiliar[2]);
-            int fuer = atoi(auxiliar[3]);
-            int intl = atoi(auxiliar[4]);
-            int vel = atoi(auxiliar[5]);
-
-            //si es un pokemon, lo creo.
             pokemon_t* pokemon = crear_pokemon(nombre, nvl, fuer, intl, vel, def);
 
             if (!pokemon) {
-                vtrfree(auxiliar);
+                vtrfree(vtr_linea_aux);
                 free(linea);
                 liberar_salon(salon);
                 fclosen(archivo);
@@ -208,34 +179,42 @@ salon_t* salon_leer_archivo(const char* nombre_archivo){
 
             salon_t* aux_salon = cargar_pokemon(salon, salon->entrenadores[pos_entrenador], pokemon);
 
-            //si falla la carga del entrenador, libero todo y retorno NULL
             if(!aux_salon){
-                vtrfree(auxiliar);
+                free(pokemon);
+                vtrfree(vtr_linea_aux);
                 free(linea);
                 liberar_salon(salon);
                 fclosen(archivo);
                 return NULL;
             }
-            //sino, actualizo la referencia. No creo que esta linea sea necesaria.
-            salon = aux_salon;
         }
+
         free(linea);
-        vtrfree(auxiliar);
+        vtrfree(vtr_linea_aux);
+
         linea = fgets_alloc(archivo);
     }
 
+    free(linea);
     fclosen(archivo);
     return salon;
 }
 
 
-int salon_guardar_archivo(salon_t* salon, const char* nombre_archivo){
+int salon_guardar_archivo(salon_t* salon, const char* nombre_archivo) {
     FILE* archivo_nuevo = fopen(nombre_archivo, "w");
     if(!archivo_nuevo)
         return -1;
 
     int cant_entrenadores_guardados = 0;
-    for(size_t i = 0; i < vtrlen(salon->entrenadores); i++) {
+
+    size_t cant_entrenadores_salon = vtrlen(salon->entrenadores);
+    if(cant_entrenadores_salon == 0){
+        fclosen(archivo_nuevo);
+        return -1;
+    }
+
+    for(size_t i = 0; i < cant_entrenadores_salon; i++) {
         entrenador_t* entrenador = salon->entrenadores[i];
 
         int linea_escrita = fprintf(archivo_nuevo, FORMATO_ESCRITURA_ENTRENADOR, entrenador->nombre, entrenador->victorias);
@@ -245,7 +224,8 @@ int salon_guardar_archivo(salon_t* salon, const char* nombre_archivo){
             return -1;
         }
 
-        for(size_t j = 0; j < vtrlen(entrenador->equipo); j++) {
+        size_t cant_pokemon_entrenador = vtrlen(entrenador->equipo);
+        for(size_t j = 0; j < cant_pokemon_entrenador; j++) {
             pokemon_t* pokemon = entrenador->equipo[j];
 
             int linea_escrita = fprintf(archivo_nuevo, FORMATO_ESCRITURA_POKEMON, pokemon->nombre, pokemon->nivel,pokemon->defensa, pokemon->fuerza, pokemon->inteligencia, pokemon->velocidad);
@@ -262,92 +242,67 @@ int salon_guardar_archivo(salon_t* salon, const char* nombre_archivo){
 }
 
 
+salon_t* ordenar_entrenadores(salon_t* salon) {
+    size_t tam = vtrlen(salon->entrenadores);
+    entrenador_t** vtr_aux_entrenadores = calloc(tam + 1, sizeof(entrenador_t*));
+
+    for (size_t i = 0; i <=  tam; i++) {
+        vtr_aux_entrenadores[i] = (entrenador_t*)salon->entrenadores[i];
+    }
+
+    entrenador_t* entrenador_aux = vtr_aux_entrenadores[tam - 1];
+
+    bool continuar = true;
+    size_t pos = 0;
+    while(vtr_aux_entrenadores[pos] && continuar && pos < tam - 1) {
+        if (entrenador_aux->victorias >= vtr_aux_entrenadores[pos]->victorias) {
+            salon->entrenadores[pos] = vtr_aux_entrenadores[pos];
+            pos++;
+        } else {
+            continuar = false;
+        }
+    }
+
+    salon->entrenadores[pos] = entrenador_aux;
+    pos++;
+
+    while(pos < tam) {
+        salon->entrenadores[pos] = vtr_aux_entrenadores[pos - 1];
+        pos++;
+    }
+
+    free(vtr_aux_entrenadores);
+
+    return salon;
+}
+
 salon_t* salon_agregar_entrenador(salon_t* salon, entrenador_t* entrenador){
     if(!salon){
-        liberar_equipo(entrenador);
         return NULL;
         }
     if(!entrenador){
-        liberar_salon(salon);
         return NULL;
     }
 
-    //si no hay entrenadores, directamente agrego el entrenador nuevo.
-    if (vtrlen(salon->entrenadores) == 0) {
-        void* entrenador_aux = vtradd(salon->entrenadores, entrenador);
+    void* entrenador_aux = vtradd(salon->entrenadores, entrenador);
 
-        if(!entrenador_aux){
-            //si fallo libero el entrenador pasado por argumento
-            liberar_equipo(entrenador);
-            //si falla el proceso, se va a devolver null, por lo que destruyo el salon
-            liberar_salon(salon);
-            return NULL;
-        }
-
-        salon->entrenadores = entrenador_aux;
-
-    //si ya hay entrenadores, debería pasar por aca
-    } else {
-        size_t posicion = 0;
-        bool continuar = true;
-        void* entrenador_aux = NULL;
-        void* vtr_dinamico_auxiliar_entrenadores = NULL;
-        //mientras me encuentre dentor del vector de entrenadores
-        while (posicion < vtrlen(salon->entrenadores) && continuar) {
-            //comparo las victorias del entrenador agregado contra el de la posicion actual. Inserto el menor.
-            if(entrenador->victorias >= salon->entrenadores[posicion]->victorias ){
-                //si el valor del archivo era menor al que estoy insertando, lo inserto primero en un vector dinamico auxiliar.
-                entrenador_aux = vtradd(vtr_dinamico_auxiliar_entrenadores, salon->entrenadores[posicion]);
-                if(!entrenador_aux) {
-                    //probablemente acá tenga que liberar algo. Por ahora solo hago un return NULL
-                    liberar_equipo(entrenador);
-                    liberar_salon(salon);
-                    vtrfree(vtr_dinamico_auxiliar_entrenadores);
-                    return NULL;
-                }
-
-                vtr_dinamico_auxiliar_entrenadores = entrenador_aux;
-                //ya que inserté el dato, me muevo a la siguiente posicion.
-                posicion++;
-            } else {
-                //como el campo original no es mayor, continuar será false. No actualizo la posicion. Si quedan registros, quiero continuar desde el actual para insertarlo luego del nuevo entrenador.
-                continuar = false;
-            }
-            //actualizo la posicion
-        }
-
-        //si sali del vector, es porque el siguiente elemento (si existe, es mayor o igual a mi). inserto el entrenador.
-        entrenador_aux = vtradd(vtr_dinamico_auxiliar_entrenadores, entrenador);
-        if(!entrenador_aux) {
-            //probablemente acá tenga que liberar algo. Por ahora solo hago un return NULL
-            liberar_equipo(entrenador);
-            liberar_salon(salon);
-            vtrfree(vtr_dinamico_auxiliar_entrenadores);
-            return NULL;
-        }
-
-        vtr_dinamico_auxiliar_entrenadores = entrenador_aux;
-
-        //ahora, si quedan, puedo insertar el resto de los entrenadores.
-        while (posicion < vtrlen(salon->entrenadores)) {
-            entrenador_aux = vtradd(vtr_dinamico_auxiliar_entrenadores, salon->entrenadores[posicion]);
-            if(!entrenador_aux) {
-                //probablemente acá tenga que liberar algo. Por ahora solo hago un return NULL
-                liberar_equipo(entrenador);
-                liberar_salon(salon);
-                vtrfree(vtr_dinamico_auxiliar_entrenadores);
-                return NULL;
-            }
-
-            vtr_dinamico_auxiliar_entrenadores = entrenador_aux;
-            posicion++;
-        }
-
-        //ya tengo mi vector auxiliar completo. Libero el vector original y luego apunto a mi nuevo vector.
-        // vtrfree(salon->entrenadores);
-
-        salon->entrenadores = vtr_dinamico_auxiliar_entrenadores;
+    if(!entrenador_aux){
+        liberar_equipo(entrenador);
+        free(entrenador);
+        return NULL;
     }
+
+    salon->entrenadores = (entrenador_t**)entrenador_aux;
+
+    if (vtrlen(salon->entrenadores) > 1) {
+        salon_t* aux  = ordenar_entrenadores(salon);
+        if(!aux){
+            liberar_equipo(entrenador);
+            free(entrenador);
+            return NULL;
+        }
+    }
+
 
     return salon;
 }
@@ -360,7 +315,6 @@ entrenador_t** salon_obtener_entrenadores_mas_ganadores(salon_t* salon, int cant
 
     void* entrenador_aux = NULL;
     for(size_t i = 0; i < vtrlen(salon->entrenadores); i++) {
-        //esto esta mal porque cada vez que ejecuto la funcion, podria recibir un null y perder la referencia a todos los datos...
         if(salon->entrenadores[i]->victorias >= cantidad_minima_victorias){
             entrenador_aux = vtradd(entrenador_aux, salon->entrenadores[i]);
         }
@@ -371,6 +325,19 @@ entrenador_t** salon_obtener_entrenadores_mas_ganadores(salon_t* salon, int cant
 }
 
 void salon_mostrar_entrenador(entrenador_t* entrenador){
+    if(!entrenador){
+        printf("No hay entrenador para mostrar\n");
+        return;
+    }
+
+    printf("Nombre: %s, Cantidad de Victorias: %d\n", entrenador->nombre, entrenador->victorias);
+    if(entrenador->equipo) {
+        printf("equipo de pokemones:\n");
+        for (size_t i = 0; i < vtrlen(entrenador->equipo); i++) {
+            pokemon_t* pokemon = entrenador->equipo[i];
+            printf("nombre: %s, nivel: %d, defensa: %d, fuerza: %d, velocidad: %d, inteligencia: %d\n", pokemon->nombre, pokemon->nivel, pokemon->defensa, pokemon->fuerza, pokemon->velocidad, pokemon->inteligencia);
+        }
+    }
 
 }
 
--- tp1_salon_de_la_fama/archivos/util.c
+++ tp1_salon_de_la_fama/archivos/util.c
@@ -8,152 +8,102 @@
 
 
 size_t vtrlen(void* ptr){
-    //si me pasan un puntero NULL, retorno 0.
     if (!ptr)
         return 0;
-
-    //sino, me creo un auxiliar de tipo void**, porque se que voy a recibir un vector dinamico para recorrer..
     void** ptr_aux = (void**)ptr;
 
     size_t cant = 0;
     size_t pos = 0;
 
-    //mientras el contenido del vector no sea NULL, voy acumulando cant
     while (ptr_aux[pos]) {
         cant++;
         pos++;
     }
-    //retorno la cant de elementos acumulada
     return cant;
 }
 
-void* vtradd(void* ptr, void* item){
-    //primero me fijo el tamaño del puntero.
-    size_t tam = vtrlen(ptr);
 
-    //me creo un puntero auxiliar del tamaño del puntero original, mas dos posiciones. Una para el item nuevo, y otra para el elemento NULL
-    // que vtrlen no tiene en cuenta al momento de dar el tamaño del vector dinamico.
+void* vtradd(void* ptr, void* item){
+    size_t tam = vtrlen(ptr);
     void** aux_ptr = realloc(ptr, (tam  + TAM_POS_CORTE_PTR +  TAM_NUEVA_POS) * sizeof(ptr));
 
-    //si falla el realloc, retorno NULL.
-    if (!aux_ptr)
+    if (!aux_ptr) {
         return NULL;
+    }
 
-    //en la nueva posicion creada, agrego el item
     aux_ptr[tam] = item;
-
-    //ahora asigno al ultimo elemento, el valor NULL.
     aux_ptr[tam + TAM_NUEVA_POS] = NULL;
-
-    //apunto al puntero original a la nueva direccion de memoria.
     ptr = (void*)aux_ptr;
 
-    //retorno el puntero
     return ptr;
 }
 
-void vtrfree(void* ptr){
 
-    //me creo un auxiliar para recorrer el vector dinamico.
+void vtrfree(void* ptr){
     void** ptr_aux = (void**)ptr;
 
-    //recorro el puntero y voy liberando su contenido. Si el puntero fuera null, el ciclo no se ejecuta porque vtrlen == 0
     for (size_t i = 0; i < vtrlen(ptr); i++) {
         free(ptr_aux[i]);
     }
-    //una vez liberado el contenido del vector dinamico, libero el puntero al vector.
     free(ptr);
 }
 
+
 char* duplicar_texto_hasta(const char* texto, size_t desde, size_t hasta) {
-    //me fijo el tamaño
     size_t tam = hasta - desde + TAM_POS_CORTE_PTR +  TAM_NUEVA_POS;
-
-    //reservo con calloc memoria por el tamaño del string a copiar. Esto es para que se inicialice en 0 cada elemento.
     char* duplicado = calloc(tam, sizeof(char));
-
-    //si falla el calloc, retorno NULL
     if(!duplicado)
         return NULL;
 
-    //sino, copio el texto desde la posicion "desde", hasta la posicion hasta - desde + 1. La última posicion que se reservó, debería quedar con un 0.
     strncpy(duplicado, texto + desde, hasta - desde + 1);
 
-    //retorno el string duplicado
     return duplicado;
 }
 
 
-
 char** split(const char* str, char separador){
-    //si el puntero es nulo, retorno NULL
     if (!str)
         return NULL;
-    //si el largo del string es 0, retorno NULL
     if (strlen(str) == 0)
         return NULL;
 
-    //primero inicializo la posicion actual del string arranco al principio de la linea.
     size_t pos_actual = 0;
-    //inicializo la cantidad de elementos que hay en el string original.
-    // Como minimo, hay un elemento, que es el que se analiza. (lo dejo en cero por ahora, ya que podria entrar en conflicto con el ultimo vtradd luego del bucle)
     size_t cant_elem = 0;
 
-    //me armo un array que contendr un vector dinamico con los strings spliteados. Por ahora apunta a NULL.
     char** split_str = NULL;
 
-    //voy a recorrer el string hasta su posicion final
     for (size_t i = 0; i < strlen(str); i++) {
-        //si encuentro el caracter de split, ejecuto las siguientes instrucciones:
         if(str[i] == separador) {
-            //aumento la cantidad de elementos. Por cada separador encontrado, siempre habrá un elemento más.
             cant_elem++;
 
-            //creo una referencia al string cortado, la duplico en memoria.
             char* texto_cortado = duplicar_texto_hasta(str, pos_actual, i-1);
-            //si falló la duplicación, debo cortar el proceso. Antes, voy a liberar el resto de textos cortados del vector y, luego, al vector.
             if (!texto_cortado) {
                 vtrfree(split_str);
                 return NULL;
             }
 
-            //si no falló, puedo agregar un elemento nuevo al vector dinámico, que contendrá el puntero al texto cortado.
             void** resultado = vtradd(split_str, texto_cortado);
-
-            //si falla el proceso de agregar el elemento, debo liberar al vector dinamico, junto con todos los elementos que hayan sido agregados correctamente.
             if (!resultado) {
                 free(texto_cortado);
                 vtrfree(split_str);
                 return NULL;
             }
 
-            //no se si es necesario realizar esto, pero el puntero que devuelve el vtradd, es void**. Lo casteo a char**
             split_str = (char**)resultado;
 
-            //una vez que terminé de procesar el string, ahora tengo que actualizar la posicion actual, se debe mover al siguiente char luego del separador.
-            //esto solo se debe realizar, mientras que la posicion actualizada, permanezca dentro del string a procesar.
-            //si la posicion queda por fuera, es porque el ultimo elemento era un separador.
             if (i < strlen(str)) {
                 pos_actual = i + 1;
             }
         }
     }
 
-    //siempre que se encontrara un separador, se cortaba todo lo anterior al separador y se armaba un string.
-    //Si tenemos 1;2;3. el primer separador va de posicion 0 a pos 0. El segundo separador, va de pos 2 a pos 2.
-    // Queda como excedente el 3, que está en posicion 4. Por ello, agregamos un elemento más, que lo va a contener.
     cant_elem++;
-
     char* texto_cortado = NULL;
 
-
-    //creamos una nueva referencia a una posicion que contiene el texto restante.
-    //Si estamos parados sobre un separador, debemos agregar un nuevo elemento vacío.
-    // Sino, agregamos el pedazo de string restante
     const char* str_aux = (str[pos_actual] == separador) ? "" : str;
     size_t pos_actual_aux = (str[pos_actual] == separador) ? 0 : pos_actual;
-    texto_cortado = duplicar_texto_hasta(str_aux, pos_actual_aux, strlen(str_aux) - 1);
 
+    texto_cortado = duplicar_texto_hasta(str_aux, pos_actual_aux, strlen(str_aux) - 1);
     if (!texto_cortado) {
         vtrfree(split_str);
         return NULL;
@@ -171,60 +121,36 @@ char** split(const char* str, char separador){
 }
 
 char* fgets_alloc(FILE* archivo){
-    //bytes leidos de la linea del archivo
     size_t bytes_leidos= 0;
-
-    //tamanio del buffer en donde voy a guardar la linea del archivo
     size_t tam_buffer = TAM_BUFFER_INI;
 
-    //reservo memoria para el buffer que estoy leyendo
     char* buffer = malloc(sizeof(char) * tam_buffer);
-
-    //si no pudo reservar, retorno NULL
     if (!buffer)
         return NULL;
 
-    //mientras que pueda leer la linea, continuo. le paso el buffer mas la cantidad de bytes leidos,
-    //le pido que lea el buffer menos la cantidad de bytes leidos, y por ultimo
-    //le paso el archivo abierto
     while (fgets(buffer + bytes_leidos, (int)(tam_buffer - bytes_leidos), archivo)){
-        //me fijo cuantos bytes lei, sumando la longitud del buffer mas la cantidad de bytes que habia leido previamente.
-        //este valor deberia ser 0 para el primer ciclo y se actualiza al finalizar cada ciclo.
         size_t leido = strlen(buffer + bytes_leidos);
-
-        //si lei algo y la anteultima posicion es un enter, retorno el buffer, porque llegué al fin de la linea
         if (leido > 0 && *(buffer + bytes_leidos + leido - 1) == '\n') {
             return buffer;
         }
-        //si llené el buffer y la anteúltima posición no es un enter, tengo que seguir leyendo, así que duplico el buffer
         else {
             char* auxiliar = realloc(buffer, sizeof(char) * tam_buffer + TAM_BUFFER_INI);
-
-            //si no pude duplicar el buffer, libero lo que tenia leido y retorno NULL
             if(!auxiliar) {
                 free(buffer);
                 return NULL;
             }
-
-            //si pude duplicar el tamaño, se lo asigno al buffer
             buffer = auxiliar;
-
-            //actualizo la variable del tamaño del buffer
             tam_buffer += TAM_BUFFER_INI;
         }
 
-
-        //actualizo la cantidad de bytes leidos.
         bytes_leidos += leido;
     }
 
-    //No pude leer nada, EOF
     if(bytes_leidos == 0) {
         free(buffer);
         return NULL;
     }
 
-    //llegue a fin de archivo pero pude leer algo
     return buffer;
 }
 
