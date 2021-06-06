cbf390b31cfb4aafa9ac062847eb076a7694f9fc
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2021-06-06 16:56:43 -0300

    Funciones y pruebas insercion y creacion abb

--- tda2_abb/abb.c
+++ tda2_abb/abb.c
@@ -1,10 +1,43 @@
 #include "abb.h"
 
-abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor){
-    return 0;
+nodo_abb_t* nodo_crear() {
+    nodo_abb_t* nodo = calloc(1, sizeof(nodo_abb_t));
+    return nodo;
 }
+
+abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor) {
+    if(!comparador)
+        return NULL;
+
+    abb_t* arbol = calloc(1, sizeof(abb_t));
+    if(arbol) {
+        arbol->comparador = comparador;
+        arbol->destructor = destructor;
+    }
+    return arbol;
+}
+
+nodo_abb_t* nodo_insertar(abb_comparador comparador, nodo_abb_t* nodo, void* elemento) {
+    if(!nodo) {
+        nodo = nodo_crear();
+        if(!nodo)
+            return NULL;
+        nodo->elemento = elemento;
+        return nodo;
+    }
+    int comparacion = comparador(elemento, nodo->elemento);
+    if(comparacion > 0)
+        nodo->derecha = nodo_insertar(comparador, nodo->derecha, elemento);
+    if(comparacion<=0)
+        nodo->izquierda = nodo_insertar(comparador, nodo->izquierda, elemento);
+    return nodo;
+}
+
 int arbol_insertar(abb_t* arbol, void* elemento){
-    return 0;
+    if(!arbol)
+        return -1;
+    arbol->nodo_raiz = nodo_insertar(arbol->comparador, arbol->nodo_raiz, elemento);
+    return (!arbol->nodo_raiz) ? -1 : 0;
 }
 int arbol_borrar(abb_t* arbol, void* elemento){
     return 0;
@@ -16,7 +49,9 @@ void* arbol_raiz(abb_t* arbol){
     return 0;
 }
 bool arbol_vacio(abb_t* arbol){
-    return 0;
+    if (!arbol)
+        return true;
+    return (!arbol->nodo_raiz);
 }
 size_t arbol_recorrido_inorden(abb_t* arbol, void** array, size_t tamanio_array){
     return 0;
@@ -27,7 +62,22 @@ size_t arbol_recorrido_preorden(abb_t* arbol, void** array, size_t tamanio_array
 size_t arbol_recorrido_postorden(abb_t* arbol, void** array, size_t tamanio_array){
     return 0;
 }
+void liberar_nodos(nodo_abb_t* nodo, abb_liberar_elemento destructor) {
+    if (!nodo)
+        return;
+    liberar_nodos(nodo->izquierda, destructor);
+    liberar_nodos(nodo->derecha, destructor);
+
+    if(destructor)
+        destructor(nodo->elemento);
+    free(nodo);
+}
+
 void arbol_destruir(abb_t* arbol){
+    if(!arbol)
+        return;
+    liberar_nodos(arbol->nodo_raiz, arbol->destructor);
+    free(arbol);
 }
 size_t abb_con_cada_elemento(abb_t* arbol, int recorrido, bool (*funcion)(void*, void*), void* extra){
     return 0;
--- tda2_abb/pruebas.c
+++ tda2_abb/pruebas.c
@@ -4,6 +4,203 @@
 #include "string.h"
 
 
+int comparar_elementos_tipo_int(void* elemento1, void* elemento2) {
+  return *(int*)elemento1 - *(int*)elemento2;
+}
+
+void destruir_elemento(void* elemento) {
+  if (elemento)
+    free(elemento);
+}
+
+void dadoUnABBNull_SiCreoUnABBSinComparador_ElABBSigueSiendoNull() {
+  abb_t* arbol_bb = NULL;
+
+  arbol_bb = arbol_crear(NULL, NULL);
+  pa2m_afirmar(arbol_bb == NULL, "El arbol sigue siendo NULL porque no se puede crear sin pasar una funcion comparadora");
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBNull_SiCreoUnABB_ResultaUnABBNoNullYVacio() {
+  abb_t* arbol_bb = NULL;
+  pa2m_afirmar(arbol_vacio(arbol_bb), "El ABB NULL esta vacio");
+  arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  pa2m_afirmar(arbol_bb != NULL, "El ABB ya no es NULL");
+  pa2m_afirmar(arbol_vacio(arbol_bb), "El ABB esta vacio");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatos_ElABBYaNoEstaVacio() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  arbol_insertar(arbol_bb, &dato1);
+  pa2m_afirmar(!arbol_vacio(arbol_bb), "El ABB ya no esta vacio");
+
+  arbol_destruir(arbol_bb);
+
+}
+
+void dadoUnABBNull_SiInsertoDatos_NoPermiteInsertarDatosEnUnABBNull (){
+  abb_t* arbol_bb = NULL;
+  int dato1 = 1;
+  pa2m_afirmar(arbol_insertar(arbol_bb, &dato1) == -1, "No permite insertar datos en un ABB NULL");
+
+  arbol_destruir(arbol_bb);
+
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenABC_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato1);
+  arbol_insertar(arbol_bb, &dato2);
+  arbol_insertar(arbol_bb, &dato3);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 1, "El nodo RAIZ tiene valor 1");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ es NULL");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->elemento == 2, "El nodo RAIZ->HIJO_DER tiene valor 2");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->izquierda == NULL, "El nodo RAIZ->HIJO_DER->HIJO_IZQ es NULL");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->derecha->elemento == 3, "El nodo RAIZ->HIJO_DER->HIJO_DER tiene valor 3");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->derecha->izquierda == NULL, "El nodo RAIZ->HIJO_DER->HIJO_DER->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->derecha->derecha == NULL, "El nodo RAIZ->HIJO_DER->HIJO_DER->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenACB_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato1);
+  arbol_insertar(arbol_bb, &dato3);
+  arbol_insertar(arbol_bb, &dato2);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 1, "El nodo RAIZ tiene valor 1");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ es NULL");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->elemento == 3, "El nodo RAIZ->HIJO_DER tiene valor 3");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->izquierda->elemento == 2, "El nodo RAIZ->HIJO_DER->HIJO_IZQ tiene valor 2");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->derecha == NULL, "El nodo RAIZ->HIJO_DER->HIJO_DER es NULL");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->izquierda->izquierda == NULL, "El nodo RAIZ->HIJO_DER->HIJO_IZQ->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->izquierda->derecha == NULL, "El nodo RAIZ->HIJO_DER->HIJO_IZQ->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenCBA_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato3);
+  arbol_insertar(arbol_bb, &dato2);
+  arbol_insertar(arbol_bb, &dato1);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 3, "El nodo RAIZ tiene valor 3");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->elemento == 2, "El nodo RAIZ->HIJO_IZQ tiene valor 2");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha == NULL, "El nodo RAIZ->HIJO_DER es NULL");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->izquierda->elemento == 1, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ tiene valor 1");
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->derecha == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_DER es NULL");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->izquierda->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->izquierda->derecha == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenCAB_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato3);
+  arbol_insertar(arbol_bb, &dato1);
+  arbol_insertar(arbol_bb, &dato2);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 3, "El nodo RAIZ tiene valor 3");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->elemento == 1, "El nodo RAIZ->HIJO_IZQ tiene valor 1");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha == NULL, "El nodo RAIZ->HIJO_DER es NULL");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ es NULL");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->derecha->elemento == 2, "El nodo RAIZ->HIJO_IZQ->HIJO_DER tiene valor 2");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->derecha->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_DER->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->derecha->derecha == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_DER->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenBAC_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato2);
+  arbol_insertar(arbol_bb, &dato1);
+  arbol_insertar(arbol_bb, &dato3);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 2, "El nodo RAIZ tiene valor 2");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->elemento == 1, "El nodo RAIZ->HIJO_IZQ tiene valor 1");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->elemento == 3, "El nodo RAIZ->HIJO_DER tiene valor 3");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->derecha == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_DER es NULL");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->izquierda == NULL, "El nodo RAIZ->HIJO_DER->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->derecha == NULL, "El nodo RAIZ->HIJO_DER->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+void dadoUnABBVacio_SiInsertoDatosEnOrdenBCA_LosDatosSeInsertanEnElOrdenCorrecto() {
+  abb_t* arbol_bb = arbol_crear(comparar_elementos_tipo_int, NULL);
+  int dato1 = 1;
+  int dato2 = 2;
+  int dato3 = 3;
+  arbol_insertar(arbol_bb, &dato2);
+  arbol_insertar(arbol_bb, &dato3);
+  arbol_insertar(arbol_bb, &dato1);
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->elemento == 2, "El nodo RAIZ tiene valor 2");
+
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->izquierda->elemento == 1, "El nodo RAIZ->HIJO_IZQ tiene valor 1");
+  pa2m_afirmar(*(int*)arbol_bb->nodo_raiz->derecha->elemento == 3, "El nodo RAIZ->HIJO_DER tiene valor 3");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->izquierda == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->izquierda->derecha == NULL, "El nodo RAIZ->HIJO_IZQ->HIJO_DER es NULL");
+
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->izquierda == NULL, "El nodo RAIZ->HIJO_DER->HIJO_IZQ es NULL");
+  pa2m_afirmar(arbol_bb->nodo_raiz->derecha->derecha == NULL, "El nodo RAIZ->HIJO_DER->HIJO_DER es NULL");
+
+  arbol_destruir(arbol_bb);
+}
+
+
 int main(){
+  pa2m_nuevo_grupo("Pruebas Creacion ABB");
+  dadoUnABBNull_SiCreoUnABBSinComparador_ElABBSigueSiendoNull();
+  dadoUnABBNull_SiCreoUnABB_ResultaUnABBNoNullYVacio();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB");
+  dadoUnABBNull_SiInsertoDatos_NoPermiteInsertarDatosEnUnABBNull();
+  dadoUnABBVacio_SiInsertoDatos_ElABBYaNoEstaVacio();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 1, 2 y 3");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenABC_LosDatosSeInsertanEnElOrdenCorrecto();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 1, 3 y 2");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenACB_LosDatosSeInsertanEnElOrdenCorrecto();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 3, 2 y 1");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenCBA_LosDatosSeInsertanEnElOrdenCorrecto();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 3, 1 y 2");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenCAB_LosDatosSeInsertanEnElOrdenCorrecto();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 2, 1 y 3");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenBAC_LosDatosSeInsertanEnElOrdenCorrecto();
+  pa2m_nuevo_grupo("Pruebas Insertar Datos en ABB. Se inserta en el siguiente orden: 2, 3 y 1");
+  dadoUnABBVacio_SiInsertoDatosEnOrdenBCA_LosDatosSeInsertanEnElOrdenCorrecto();
   return pa2m_mostrar_reporte();
 }
