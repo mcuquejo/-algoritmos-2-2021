bd9d3edc29718b89f45f521f322bae220c4237b1
MAURO CUQUEJO <mcuquejo@fi.uba.ar>
2021-07-04 17:38:13 -0300

    Se agrega funcionalidad creacion, insercion y busqueda hash

--- tda3_hash/hash.c
+++ tda3_hash/hash.c
@@ -1,37 +1,128 @@
 #include <string.h>
 #include <stdlib.h>
 #include "hash.h"
+#include <stdio.h>
 
-struct hash{
+typedef struct elemento {
+  const char* clave;
+  void* valor;
+} elemento_t;
+
+struct hash {
+  hash_destruir_dato_t destructor;
+  size_t cantidad_elementos;
+  size_t capacidad_total;
+  elemento_t** elementos;
 };
 
 
+long fnv_hashing(const char* clave) {
+  long h = 1469591039346656037;
+  int n = strlen(clave);
+  for (size_t i = 0; i < n; i++) {
+    h *= 1099511628211;
+    h ^= clave[i];
+  }
+  return h;
+}
+
+int calcular_posicion(const char* clave, size_t tamanio_hash){
+  return fnv_hashing(clave) % tamanio_hash;
+}
+
+elemento_t* elemento_crear(const char* clave, void* valor) {
+  elemento_t* elemento = malloc(sizeof(elemento_t));
+  if(!elemento)
+    return NULL;
+
+  elemento->clave = clave;
+  elemento->valor = valor;
+  return elemento;
+}
+
 hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inicial){
-  return NULL;
+  hash_t* hash = calloc(1, sizeof(hash_t));
+  if(!hash)
+    return NULL;
+  hash->destructor = destruir_elemento;
+  hash->capacidad_total = (capacidad_inicial >= 3) ? capacidad_inicial : 3;
+  hash->elementos = calloc(capacidad_inicial, sizeof(elemento_t));
+  if(!hash->elementos){
+    free(hash);
+    return NULL;
+  }
+  return hash;
 }
 
 
 int hash_insertar(hash_t* hash, const char* clave, void* elemento){
+  if(!hash || !clave)
+    return -1;
+
+  size_t posicion = calcular_posicion(clave, hash->capacidad_total);
+
+  //rehashear en algun momento.
+
+  if(!hash->elementos[posicion]) {
+    elemento_t* elemento_nuevo = elemento_crear(clave, elemento);
+    if(!elemento)
+      return -1;
+    hash->elementos[posicion] = elemento_nuevo;
+    //return 0;
+  } else if (strcmp(hash->elementos[posicion]->clave, clave) == 0) {
+      void* auxiliar = hash->elementos[posicion]->valor;
+      hash->elementos[posicion]->valor = elemento;
+      if(hash->destructor)
+        hash->destructor(auxiliar);
+  }
+
+  hash->cantidad_elementos++;
+
   return 0;
 }
 
-int hash_quitar(hash_t* hash, const char* clave){
+int hash_quitar(hash_t* hash, const char* clave) {
   return 0;
+}
+
+elemento_t* elemento_obtener(hash_t* hash, size_t pos, const char* clave) {
+  if (!hash->elementos[pos] || pos > hash->capacidad_total)
+    return NULL;
+  if(strcmp(hash->elementos[pos]->clave, clave) == 0)
+    return hash->elementos[pos];
+  return elemento_obtener(hash, pos++, clave);
 }
 
 void* hash_obtener(hash_t* hash, const char* clave){
-  return NULL;
+  if(!hash || !clave)
+    return NULL;
+  size_t posicion = calcular_posicion(clave, hash->capacidad_total);
+  elemento_t* elemento_buscado = elemento_obtener(hash, posicion, clave);
+  return elemento_buscado->valor;
 }
 
-size_t hash_cantidad(hash_t* hash){
-  return 0;
+size_t hash_cantidad(hash_t* hash) {
+  return (hash) ? hash->cantidad_elementos : 0;
 }
+
 
 bool hash_contiene(hash_t* hash, const char* clave){
-  return false;
+  if(!hash || !clave)
+    return false;
+  size_t posicion = calcular_posicion(clave, hash->capacidad_total);
+  elemento_t* elemento_buscado = elemento_obtener(hash, posicion, clave);
+  return (elemento_buscado != NULL);
 }
 
-void hash_destruir(hash_t* hash){
+void hash_destruir(hash_t* hash) {
+  for(size_t i = 0; i < hash->capacidad_total; i++) {
+    if(hash->destructor) {
+      hash->destructor(hash->elementos[i]->valor);
+    }
+    free(hash->elementos[i]);
+  }
+  free(hash->elementos);
+  free(hash);
 }
 
 size_t hash_con_cada_clave(hash_t* hash, bool (*funcion)(hash_t* hash, const char* clave, void* aux), void* aux){
--- tda3_hash/hash_pruebas.c
+++ tda3_hash/hash_pruebas.c
@@ -1,7 +1,103 @@
 #include "hash.h"
 #include <stdio.h>
 #include "pa2mm.h"
+#include <string.h>
+
+#define CANTIDAD 10
+
+void dadoUnHashNull_SiCreoUnHash_ElHashSeCreaCorrectamente() {
+  hash_t* hash = hash_crear(NULL, CANTIDAD);
+  pa2m_afirmar(hash, "El hash no es NULL. Se creo correctamente");
+  pa2m_afirmar(hash_cantidad(hash) == 0, "El hash se crea vacio");
+  hash_destruir(hash);
+}
+
+void dadoUnHashVacio_SiInsertoElementos_ElHashYaNoEstaVacio() {
+  hash_t* hash = hash_crear(NULL, CANTIDAD);
+  const char* clave = "102624";
+  const char* valor = "MAURO CUQUEJO";
+  const char* clave_2 = "102625";
+  const char* valor_2 = "JORGITO PEREZ";
+  hash_insertar(hash, clave, &valor);
+  pa2m_afirmar(hash_cantidad(hash) == 1, "El hash ahora contiene un elemento");
+  hash_insertar(hash, clave_2, &valor_2);
+  pa2m_afirmar(hash_cantidad(hash) == 2, "El hash ahora contiene dos elementos");
+  hash_destruir(hash);
+}
+
+void dadoUnHashNULL_SiVerificoExistenciaElementosEnElHash_NoEncuentroLosElementos() {
+  hash_t* hash = NULL;
+  const char* clave_1 = "102624";
+  pa2m_afirmar(!hash_contiene(hash, clave_1), "No se encuentra la clave 1 en el hash. Porque el hash es NULL");
+}
+
+void dadoUnHashSinElementos_SiVerificoExistenciaElementosEnElHash_NoEncuentroLosElementos() {
+  hash_t* hash = hash_crear(NULL, CANTIDAD);
+  const char* clave_1 = "102624";
+  pa2m_afirmar(!hash_contiene(hash, clave_1), "No se encuentra la clave 1 en el hash. Porque el hash no tiene elementos");
+  hash_destruir(hash);
+
+}
+
+void dadoUnHashConElementos_SiVerificoExistenciaElementosQueSiExistenEnElHash_EncuentroLosElementos() {
+  hash_t* hash = hash_crear(NULL, CANTIDAD);
+  const char* clave_1 = "102624";
+  const char* valor_1 = "MAURO CUQUEJO";
+  const char* clave_2 = "102625";
+  const char* valor_2 = "JORGITO PEREZ";
+  const char* clave_3 = "102626";
+  const char* valor_3 = "MAXIMO COSSETTI";
+
+  hash_insertar(hash, clave_1, &valor_1);
+  hash_insertar(hash, clave_2, &valor_2);
+  hash_insertar(hash, clave_3, &valor_3);
+
+  pa2m_afirmar(hash_contiene(hash, clave_1), "Se encuentra la clave 1 en el hash");
+  pa2m_afirmar(hash_contiene(hash, clave_2), "Se encuentra la clave 2 en el hash");
+  pa2m_afirmar(hash_contiene(hash, clave_3), "Se encuentra la clave 3 en el hash");
+
+  hash_destruir(hash);
+}
+
+void dadoUnHashConElementos_SiObtengoValorDeElementosQueSiExistenEnElHash_ReciboElValorCorrectoDeLosElementos() {
+  hash_t* hash = hash_crear(NULL, CANTIDAD);
+
+  const char* clave_1 = "102624";
+  const char* valor_1 = "MAURO CUQUEJO";
+  const char* clave_2 = "102625";
+  const char* valor_2 = "JORGITO PEREZ";
+  const char* clave_3 = "102626";
+  const char* valor_3 = "MAXIMO COSSETTI";
+
+  hash_insertar(hash, clave_1, &valor_1);
+  hash_insertar(hash, clave_2, &valor_2);
+  hash_insertar(hash, clave_3, &valor_3);
+
+  pa2m_afirmar(strcmp(*(char**)hash_obtener(hash, clave_1), valor_1) == 0, "Se obtiene correctamente el valor de la clave 1");
+  pa2m_afirmar(strcmp(*(char**)hash_obtener(hash, clave_2), valor_2) == 0, "Se obtiene correctamente el valor de la clave 2");
+  pa2m_afirmar(strcmp(*(char**)hash_obtener(hash, clave_3), valor_3) == 0, "Se obtiene correctamente el valor de la clave 3");
+
+  const char* valor_1_modificado = "FRANCO MILAZZO";
+
+  hash_insertar(hash, clave_1, &valor_1_modificado);
+  pa2m_afirmar(strcmp(*(char**)hash_obtener(hash, clave_1), valor_1) != 0, "Se modifico valor de clave 1. Ahora el valor del hash es distinto al del valor 1");
+  pa2m_afirmar(strcmp(*(char**)hash_obtener(hash, clave_1), valor_1_modificado) == 0, "Al haber modificado el valor de clave 1, ahora el valor del hash es igual al del valor 1 modificado");
+
+  hash_destruir(hash);
+}
 
 int main(){
+  pa2m_nuevo_grupo("Pruebas Creacion Hash");
+  dadoUnHashNull_SiCreoUnHash_ElHashSeCreaCorrectamente();
+  pa2m_nuevo_grupo("Pruebas Insertar elementos en Hash sin funcion destruccion");
+  dadoUnHashVacio_SiInsertoElementos_ElHashYaNoEstaVacio();
+  pa2m_nuevo_grupo("Pruebas verificar existencia elementos en Hash NULL");
+  dadoUnHashNULL_SiVerificoExistenciaElementosEnElHash_NoEncuentroLosElementos();
+  pa2m_nuevo_grupo("Pruebas verificar existencia elementos en Hash Vacio");
+  dadoUnHashSinElementos_SiVerificoExistenciaElementosEnElHash_NoEncuentroLosElementos();
+  pa2m_nuevo_grupo("Pruebas verificar existencia elementos insertados en Hash");
+  dadoUnHashConElementos_SiVerificoExistenciaElementosQueSiExistenEnElHash_EncuentroLosElementos();
+  pa2m_nuevo_grupo("Pruebas valores de elementos insertados en Hash");
+  dadoUnHashConElementos_SiObtengoValorDeElementosQueSiExistenEnElHash_ReciboElValorCorrectoDeLosElementos();
   return pa2m_mostrar_reporte();
 }
