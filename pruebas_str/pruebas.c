861ab6f4843d1aa896783c8de198dc6a00d834ae
MAURO CUQUEJO <mcuquejo@fi.uba.ar>
2021-08-18 08:32:02 -0300

    Se agregan pruebas concatenacion pokemon y entrenador

--- pruebas_str/pruebas.c
+++ pruebas_str/pruebas.c
@@ -1,11 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-
-char* concat(char *s1, const char *s2)
+typedef enum tipo
 {
-    if(!s1) {
+    ROCA,
+    AGUA,
+    ELECTRICO,
+    PLANTA,
+    VENENO,
+    FUEGO,
+    PSIQUICO,
+    TIERRA
+} pokemon_tipo_t;
+
+char *concat(char *s1, const char *s2)
+{
+    if (!s1) {
         s1 = calloc(1, sizeof(strlen(s2) + 1));
         strcpy(s1, s2);
         return s1;
@@ -17,28 +29,27 @@ char* concat(char *s1, const char *s2)
     return s1;
 }
 
+void pruebas_str()
+{
+    const char *a = "these ";
+    const char *b = "strings ";
+    const char *c = "are ";
+    const char *d = "concatenated.";
 
-int main() {
-
-    const char* a = "these ";
-    const char* b = "strings ";
-    const char* c = "are ";
-    const char* d = "concatenated.";
-
-    const char** string = calloc(4, sizeof(char*));
+    const char **string = calloc(4, sizeof(char *));
 
     string[0] = a;
     string[1] = b;
     string[2] = c;
     string[3] = d;
 
-    for(size_t i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 4; i++) {
         printf("%s", string[i]);
     }
     printf("\n");
 
     char *stringer = NULL;
-    for(size_t i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 4; i++) {
         concat(stringer, string[i]);
     }
 
@@ -54,5 +65,17 @@ int main() {
     printf("%s\n", str);
     free(str);
     free(stringer);
+}
+
+int main()
+{
+    // pruebas_str();
+    srand((unsigned int)time(NULL));
+    int r = rand() % 10;
+    printf("%i\n", r);
+
+    pokemon_tipo_t tipo = FUEGO;
+
+    printf("%d\n", tipo);
     return 0;
 }
